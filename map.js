7ea77f38a7ae3da5ac1c52ae849ccf7c86557d00
capozzic1 <capozzic1@gmail.com>
2017-03-22 10:44:03 +0900

    Add Map files

---
+++ map.js
@@ -0,0 +1,9 @@
+    var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 36.166, lng: -115.138},
+          zoom: 14
+        });
+      }
+	  
+	  //36.166902, -115.138241
