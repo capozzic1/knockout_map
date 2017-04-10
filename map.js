dfe70ff8d1d0a6414915e8c8f842fac5f0a6a969
capozzic1 <capozzic1@gmail.com>
2017-04-10 14:34:32 +0900

    Add some location data

--- map.js
+++ map.js
@@ -1,9 +1,27 @@
-    var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 36.166, lng: -115.138},
-          zoom: 14
-        });
-      }
+var Module = (function () {
 	  
-	  //36.166902, -115.138241
+	  //model data
+	  var locations = [
+	  {name:'Le Thai', coords:{lat:36.168743, lng:-115.139866}},
+	  {name:'Atomic Liquors', coords:{lat:36.166782, lng:-115.13551}},
+      {name:'The Griffin', coords:{lat:36.168785, lng:-115.140329}},
+	  {name:'Pizza Rock', coords:{lat:36.17182, lng:-115.142304}}
+	
+	  ]
+	  
+      var initMap = function () {
+	    var map;
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat:  36.167059, lng: -115.135555},
+          zoom: 15
+        });
+      };
+	  
+	  return { 
+		initMap: initMap
+	  };
+	  
+	  
+	  
+	
+})();
