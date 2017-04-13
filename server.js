d6deb91066c9685c661867ed388bce83d2b0a1b6
capozzic1 <capozzic1@gmail.com>
2017-04-13 20:50:16 +0900

    Updated debugging

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var Module = (function() {
     'use strict';
+	//var levels = require('debug-levels').levels;
+	var debug = require('debug')('app');
     var prettyjson = require('prettyjson');
     var express = require('express');
     var http = require('http');
     var cors = require('cors');
     var bodyParser = require('body-parser');
     var app = express();
-
+	//var dbg = debug('myserver:app');
+	var name = "server";
+	var yelpData = [];
     const yelp = require('yelp-fusion');
 
+	
+	debug('testing');
     const token = yelp.accessToken('euqH0_vzVDHpkWNkOrRvRg', 'zBM0qybw8qfqd6Rp9l9qef3i8niXOfM2oft0IHx68yxhBPNfehHA0r6pfULQunN9').then(response => {
         // console.log(response.jsonBody.access_token);
     }).catch(e => {
@@ -33,9 +39,12 @@ var Module = (function() {
     //handle post requests
     app.post('/', function(req, res) {
 		var test = req.body[0].name;
-		console.log(test);
-		debugger;
-		console.log("TESSST");
+		//console.log(test);
+		req.body.forEach((item) => {
+			yelpData.push(item);
+		});
+		
+		
         //var jsonData = JSON.parse(reqbody);
 
 
