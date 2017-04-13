9be88ed7a07b8f309d5f98600ce86b4b2308001b
Chris <capozzic1@gmail.com>
2017-04-13 13:10:37 +0900

    Updated files

---
+++ server.js
@@ -0,0 +1,47 @@
+var Module = (function() {
+    'use strict';
+    var prettyjson = require('prettyjson');
+    var express = require('express');
+    var http = require('http');
+    var cors = require('cors');
+    var bodyParser = require('body-parser');
+    var app = express();
+
+    const yelp = require('yelp-fusion');
+
+    const token = yelp.accessToken('euqH0_vzVDHpkWNkOrRvRg', 'zBM0qybw8qfqd6Rp9l9qef3i8niXOfM2oft0IHx68yxhBPNfehHA0r6pfULQunN9').then(response => {
+        // console.log(response.jsonBody.access_token);
+    }).catch(e => {
+        console.log(e);
+    });
+    //allow cors
+    app.use(cors());
+    // create application/json parser 
+    app.use(bodyParser.json());
+    // create application/x-www-form-urlencoded parser 
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+
+
+
+    app.listen(3000, function() {
+        console.log('Example app listening on port 3000');
+    });
+
+    //handle post requests
+    app.post('/', function(req, res) {
+		var test = req.body[0].name;
+		console.log(test);
+		debugger;
+		console.log("TESSST");
+        //var jsonData = JSON.parse(reqbody);
+
+
+        //console.log(typeof req.body);
+        //console.log(prettyjson.render(test));
+
+    });
+
+})();
