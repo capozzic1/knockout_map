c39e4973871281885d7097945fa2533c28bbbcfb
Chris <capozzic1@gmail.com>
2018-02-12 18:34:58 +0900

    Updated folder structure

---
+++ src/public/server.js
@@ -0,0 +1,82 @@
+var Module = (function() {
+  'use strict';
+  var Promise = require('promise');
+  var cors = require('cors');
+  var debug = require('debug')('app');
+  var express = require('express');
+  var http = require('http');
+  var bodyParser = require('body-parser');
+  var app = express();
+  var name = "server";
+  var yelpInData = [];
+
+  var port = process.env.PORT || 8080;
+
+  const yelp = require('yelp-fusion');
+
+  app.use(express.static('public'));
+
+  //const client = yelp.client(token);
+  app.listen(port, function() {
+    console.log('Example app listening on port' + port);
+  });
+
+  //allow cors
+  app.use(cors());
+  // create application/json parser
+  app.use(bodyParser.json());
+  // create application/x-www-form-urlencoded parser
+  app.use(bodyParser.urlencoded({extended: true}));
+
+  //handle post requests
+  app.post('/p', async function(req, res) {
+
+    req.body.forEach((item) => {
+      yelpInData.push(item);
+    });
+
+    let result = await handleYelp(yelpInData)
+    console.log(result);
+
+    res.send(result);
+
+  });
+
+  async function handleYelp(yelpInData) {
+    return new Promise(resolve => {
+      yelp.accessToken('euqH0_vzVDHpkWNkOrRvRg', 'zBM0qybw8qfqd6Rp9l9qef3i8niXOfM2oft0IHx68yxhBPNfehHA0r6pfULQunN9').then(response => {
+        const client = yelp.client(response.jsonBody.access_token);
+
+        var yelpPromises = [];
+
+        yelpInData.forEach(async (item, i) => {
+          let coords = yelpInData[i].coords.lat + "," + yelpInData[i].coords.lng;
+          let name = yelpInData[i].name;
+
+          await client.search({term: name, location: coords}).then(response => {
+
+            //name, img url, review count, rating, price, location.display address, is closed,phone
+            yelpPromises.push(resolve({
+              name: response.jsonBody.businesses[0].name,
+              img: response.jsonBody.businesses[0].image_url,
+              hours: response.jsonBody.businesses[0].is_closed,
+              revcount: response.jsonBody.businesses[0].review_count,
+              rating: response.jsonBody.businesses[0].rating,
+              price: response.jsonBody.businesses[0].price,
+              location: response.jsonBody.businesses[0].location.display_address.toString(),
+              phone: response.jsonBody.businesses[0].display_phone,
+              url: response.jsonBody.businesses[0].url
+            }));
+
+            console.log(yelpPromises);
+          }).catch(e => {
+            console.log(e);
+          });
+        });
+
+      }).catch(e => {
+        console.log(e);
+      });
+    })
+  }
+})();
